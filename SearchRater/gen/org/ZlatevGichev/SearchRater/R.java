8b918267864549ff3be8177a94507293e4f2a587
Blagovest Zlatev <blagozlatev@gmail.com>
2012-04-29 18:03:35 +0300

    Add new icons for all resolutions

--- SearchRater/gen/org/ZlatevGichev/SearchRater/R.java
+++ SearchRater/gen/org/ZlatevGichev/SearchRater/R.java
@@ -12,7 +12,6 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int search=0x7f020001;
     }
     public static final class id {
         public static final int btnSearch=0x7f050001;
