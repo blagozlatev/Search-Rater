4b36d7169bb6ad08b6e063633ec2302ff3391771
Blagovest Zlatev <blagozlatev@gmail.com>
2012-04-30 11:11:42 +0300

    Add test class and lock orientation for GUI

---
+++ SearchRaterTest/src/org/ZlatevGichev/SearchRater/test/SearchRaterTest.java
@@ -0,0 +1,19 @@
+package org.ZlatevGichev.SearchRater.test;
+
+import org.ZlatevGichev.SearchRater.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class SearchRaterTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	public SearchRaterTest(Class<MainActivity> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();		
+	}
+}
