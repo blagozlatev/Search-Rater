49302e5dfdbee691764f8a59117c2e39e936894e
Nikolay Gichev <niki1312@abv.bg>
2012-05-13 16:24:23 +0300

    Refractored arguments, and cursor

--- SearchRaterTest/Test/org/ZlatevGichev/SearchRater/DatabaseHandlerTest.java
+++ SearchRaterTest/Test/org/ZlatevGichev/SearchRater/DatabaseHandlerTest.java
@@ -10,36 +10,36 @@ import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
 
-
 public class DatabaseHandlerTest extends AndroidTestCase {
 
 	private static final String TABLE_BLOCKED_RESULTS = "blocked_results";
 	private static final String KEY_SEARCH_QUERY = "search_query";
+	private static final String KEY_LINK = "link";
 
 	private DatabaseHandler databaseHandler1;
 	private Context context_1;
 	SQLiteDatabase db;
-	
+
 	@Override
 	 protected void setUp() throws Exception {
 	  super.setUp();
 	  context_1 = getContext();
 	  databaseHandler1 = new DatabaseHandler(context_1);
 	 }
-	
+
 	@Test
 	 public void testDeleteLink() throws Throwable {
-		 String searchQuery = "blago"; 
-		 databaseHandler1.addLink(searchQuery, "www.google.com");
+		 String searchQuery = "krasi"; 
+		 String URL = "www.google.com";
+		 databaseHandler1.addLink(searchQuery, URL);
 		 setUp();
 		 db = databaseHandler1.getWritableDatabase();
 		 Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_BLOCKED_RESULTS
-		 		 + " WHERE " + KEY_SEARCH_QUERY + "=" + "'" + searchQuery + "'", 
-		 		  null);
+		 		 + " WHERE " + KEY_LINK + "=" + "'" + URL + "'", 
+		 		 null);
 		 if(cursor.moveToFirst()) {
-			 databaseHandler1.deleteLink("www.google.com");
+			 databaseHandler1.deleteLink(URL);
 			 assertFalse(cursor.moveToNext());
 		 }
-		 db.close();
-	 }
+	 } 
 }
