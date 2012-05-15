0550b30b4cd55acd2c74528e3dd342455db23407
Nikolay Gichev <niki1312@abv.bg>
2012-05-15 09:19:05 +0300

    Added test for GetResultsFromJSON and changed Google Custom Search key in urlToReturn

--- SearchRaterTest/Test/org/ZlatevGichev/SearchRater/TestJSONHandler.java
+++ SearchRaterTest/Test/org/ZlatevGichev/SearchRater/TestJSONHandler.java
@@ -1,33 +1,28 @@
 package org.ZlatevGichev.SearchRater;
 
+
 import java.io.ByteArrayInputStream;
 
 import java.io.InputStream;
-
-
+import java.util.ArrayList;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
 
+import android.os.Bundle;
 import android.test.AndroidTestCase;
-
 
 
 public class TestJSONHandler extends AndroidTestCase{
 
-	String urlToReturn = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCZI07EqEqZJUOTX_yDGRFsuBzHseo3WRo&cx=013036536707430787589:_pqjad5hr1a&q=Blagovest%20Zlatev&alt=json";
+	String urlToReturn = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBJWWAjCjFy-JnrYYkwDJJwrUwJYtzEDTk&cx=013036536707430787589:_pqjad5hr1a&q=Blagovest%20Zlatev&alt=json";
 	String searchQuery = "Blagovest Zlatev";
-	
-	@Before
-	public void setUp() throws Exception {
-
-	}
 	
 	@Test 
 	public void testConnectAndGetJSON() {
 		try {
+			
 			JSONObject test = JSONHandler.connectAndGetJSON("www.dir.bg"); 
 			JSONObject test1 = JSONHandler.connectAndGetJSON("www.google.com"); 
 			JSONObject test2 = JSONHandler.connectAndGetJSON("www.google.com"); 
@@ -44,6 +39,22 @@ public class TestJSONHandler extends AndroidTestCase{
 		}
 	}
 
+	@Test
+	public void testGetResultsFromJSON () {
+		
+		ArrayList<Bundle> test =  JSONHandler.getResultsFromJSON("Alfa Romeo");
+		ArrayList<Bundle> test1 = JSONHandler.getResultsFromJSON("BMW");
+		ArrayList<Bundle> test2 = JSONHandler.getResultsFromJSON("Alfa Romeo");
+		assertNotNull(test);
+		assertNotNull(test1);
+		assertNotNull(test2);
+		assertNotNull(test.get(2).getString("title"));
+		assertNotNull(test1.get(2).getString("title"));
+		assertNotNull(test2.get(2).getString("title"));
+		assertNotSame(test.get(2).getString("title"),test1.get(2).getString("title"));
+		assertEquals(test.get(2).getString("title"), test2.get(2).getString("title"));
+		
+	}
 	@Test
 	public void testConvertSearchQueryToURL() {
 
