d11c488b499439eef86bee32e8dc30dda7293f81
Nikolay Gichev <niki1312@abv.bg>
2012-05-16 13:14:58 +0300

    Added activity test and removed variables and imports which is never used.

--- SearchRaterTest/Test/org/ZlatevGichev/SearchRater/MainActivityTest.java
+++ SearchRaterTest/Test/org/ZlatevGichev/SearchRater/MainActivityTest.java
@@ -1,74 +1,83 @@
 package org.ZlatevGichev.SearchRater;
 
 
-import java.util.ArrayList;
+import junit.framework.Assert;
 
+import org.ZlatevGichev.SearchRater.MainActivity;
 import org.junit.Test;
 
-import android.content.Context;
+import com.jayway.android.robotium.solo.Solo;
+
+
 import android.os.Bundle;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 
 
 public class MainActivityTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
-		private String testString;
-		private  MainActivity mainActivity;
-		private TextView editText;
-		private ListView resultList;
-		private	Button btnSearch;
-		private DatabaseHandler databaseHandler;
-		private Context context;
-		ArrayList<Bundle> bundledNamesAndLinks = new ArrayList<Bundle>();
-		Bundle testBundle;
 		
-	public MainActivityTest() {
-		super("org.ZlatevGichev.SearchRater", MainActivity.class);
+		private  MainActivity mainActivity;
+		private Solo solo;
+		Bundle testBundle;
+
+	public MainActivityTest() {		
+		super("org.ZlatevGichev.SearchRater", MainActivity.class);	
 	}
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		databaseHandler = new DatabaseHandler(context);
+		
+		solo = new Solo(getInstrumentation(), getActivity());
 		testBundle = new Bundle();
-		mainActivity = getActivity();  
-		editText = (TextView) mainActivity.findViewById(R.id.editText);
-		resultList = (ListView) mainActivity.findViewById(R.id.resultList);
-		btnSearch = (Button) mainActivity.findViewById(R.id.btnSearch);
-		testString = mainActivity.getString(R.id.editText);
+		mainActivity = getActivity();
+	}
+	@Test
+	public void testBlockedAndUnblockedResults() throws Exception {
+		  	setUp();
+		  	solo.clickOnText("OK");
+		  	solo.enterText(0, "Alfa Romeo");
+		  	solo.clickOnButton("Search");
+		  	solo.clickInList(1);
+		  	Assert.assertTrue(solo.searchText("Alfa Romeo Australia")); //first result is blocked
+		  	solo.clickLongInList(2); // second result is blocked
+		  	solo.clickInList(2);
+		  	Assert.assertTrue(solo.searchText("Alfa Romeo Australia"));
+		  	solo.clickLongInList(2); // second result is unblocked
+		  	Assert.assertTrue(solo.searchText("Alfa Romeo Australia")); 
+		  	solo.goBack();
+		  	tearDown();
+	}	
+
+	@Override
+	public void tearDown() throws Exception {
+			solo.finishOpenedActivities();
 	}
 
 	@Test
 	public void testGetTitleForLink() throws Exception {
-		
 		setUp();
 		testBundle.putString("title", "Alfa Romeo");
 		MainActivity.bundledNamesAndLinks.add(0, testBundle);
 		String testGetTitle = mainActivity.getTitleForLink(0);
 		assertEquals(MainActivity.bundledNamesAndLinks.get(0).getString("title"), testGetTitle);
 	}
+
 	@Test
 	public void testGetLinkFromBundle() throws Exception {
-		
 		setUp();
 		testBundle.putString("link", "www.google.com");
 		MainActivity.bundledNamesAndLinks.add(0, testBundle);
 		String testGetLink = mainActivity.getLinkFromBundle(0);
 		assertEquals(MainActivity.bundledNamesAndLinks.get(0).getString("link"), testGetLink);
-		
 	}
+
 	@Test
 	public void testGetSearchQueryForBundle() throws Exception {
-		
 		setUp();
 		testBundle.putString("search_query", "Alfa");
 		MainActivity.bundledNamesAndLinks.add(0, testBundle);
 		String testGetSearchQuery = mainActivity.getSearchQueryForBundle();
 		assertEquals(MainActivity.bundledNamesAndLinks.get(0).getString("search_query"), testGetSearchQuery);
-		
 	}
-	
 }
